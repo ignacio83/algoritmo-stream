532e249ea3d56e124349c95daedbdf2e1e9b58d9
Andre de Fontana Ignacio <ignacio83@gmail.com>
2015-09-07 21:33:59 -0300

    Commit inicial

---
+++ src/main/java/br/com/afi/algoritmo/Stream.java
@@ -0,0 +1,12 @@
+package br.com.afi.algoritmo;
+
+/**
+ * Representa uma Stream.
+ * 
+ * @author André de Fontana Ignacio
+ * @version 1.0
+ */
+public interface Stream {
+	char getNext();
+	boolean hasNext();
+}
